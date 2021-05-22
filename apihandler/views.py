724083c18eec4a845a91f363bc2ad03ec1dcc37a
CKPatil <ckpatil0206@gmail.com>
2021-05-23 01:38:33 +0530

    test 2

--- apihandler/views.py
+++ apihandler/views.py
@@ -1,7 +1,9 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World");
+    a=request.get['http://dummy.restapiexample.com/api/v1/employees']
+
+    return render(request,'home.html',{'name':a})
